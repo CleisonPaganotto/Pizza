e6dc39bc21ea4fb0ac72d6b57dfd8f2515543c80
Cleison Paganotto <cleisonpaganotto2014@gmail.com>
2022-04-08 08:48:29 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,57 @@
+let pizzaJson = [{
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        price: 20.50,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, mussarela, orégano, azeitona e tomate'
+    },
+    {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: 25.25,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, Calabresa, mussarela, orégano, azeitona e tomate'
+    },
+    {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: 30.35,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, mussarela, parmesão, provolone, gorgonzola, orégano, azeitona e tomate'
+    },
+    {
+        id: 4,
+        name: 'Americana',
+        img: 'images/pizza4.png',
+        price: 35.50,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, mussarela, carne, milho, presunto, orégano, azeitona e tomate'
+    },
+    {
+        id: 5,
+        name: 'Sorvete',
+        img: 'images/pizza5.png',
+        price: 40.40,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, chocolate, Sorvete, orégano, azeitona e tomate'
+    },
+    {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: 45.45,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por molho de tomate, mussarela, frango, bacon, requeijão, orégano, azeitona e tomate'
+    },
+    {
+        id: 7,
+        name: 'Chocolate',
+        img: 'images/pizza7.png',
+        price: 50.50,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Massa a base de trigo e agua, recheio composto por leite condensado, chocolate branco, chocolate ao leite'
+    }
+];
